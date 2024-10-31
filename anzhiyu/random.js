5cf6190f26ea5ed30834aedd67a37531dff8df8a
rxl1231 <rxl1231@163.com>
2024-10-31 20:34:03 +0800

    Site updated: 2024-10-31 20:34:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/29/hello-world/"];function toRandomPost(){
+var posts=["2024/10/31/公司简介/","2024/10/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
