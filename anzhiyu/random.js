93b6bf87285abb86c1d4a612746844676496785d
rxl1231 <rxl1231@163.com>
2024-11-01 17:36:54 +0800

    Site updated: 2024-11-01 17:36:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/31/公司简介/","2024/10/29/hello-world/"];function toRandomPost(){
+var posts=["2024/11/01/拓福轴承商标/","2024/10/31/公司简介/","2024/10/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
